9dc2cae77ad9c8cc519989fa12b01d46b84d4052
shoto13 <wilsonrc@pm.me>
2022-01-15 23:50:58 +0000

    start work on tagline update functionality

--- app/src/main/java/com/journey13/exchainge/settingsChangeTagline.java
+++ app/src/main/java/com/journey13/exchainge/settingsChangeTagline.java
@@ -3,12 +3,45 @@ package com.journey13.exchainge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class settingsChangeTagline extends AppCompatActivity {
+
+    private Button changeTaglineButton;
+    private EditText changeTaglineEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_change_tagline);
+
+        changeTaglineButton = findViewById(R.id.confirmChangesButton);
+        changeTaglineEditText = findViewById(R.id.changeTaglineEditText);
+
+        changeTaglineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(TextUtils.isEmpty(changeTaglineEditText.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "You must enter your new tag line in the field above", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    String newTag = changeTaglineEditText.getText().toString();
+                    setNewTagline(newTag);
+                }
+            }
+        });
+
+
     }
+
+    public void setNewTagline(String tagline) {
+        //TODO UPDATE TAGLINE IN FIREBASE WITHIN THIS FUNCTION
+    }
+
+
 }
