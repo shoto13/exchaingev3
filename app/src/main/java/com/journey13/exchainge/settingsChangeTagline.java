eaf606c23e29ffd0f16cf7c6f95b894203e05f27
shoto13 <wilsonrc@pm.me>
2022-01-23 09:37:21 +0000

    Complete update tagline functionality

--- app/src/main/java/com/journey13/exchainge/settingsChangeTagline.java
+++ app/src/main/java/com/journey13/exchainge/settingsChangeTagline.java
@@ -1,5 +1,6 @@
 package com.journey13.exchainge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,10 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class settingsChangeTagline extends AppCompatActivity {
 
     private Button changeTaglineButton;
     private EditText changeTaglineEditText;
+
+    private DatabaseReference reference;
+    private FirebaseUser fuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,8 @@ public class settingsChangeTagline extends AppCompatActivity {
 
         changeTaglineButton = findViewById(R.id.confirmChangesButton);
         changeTaglineEditText = findViewById(R.id.changeTaglineEditText);
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
 
         changeTaglineButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +44,6 @@ public class settingsChangeTagline extends AppCompatActivity {
                 if(TextUtils.isEmpty(changeTaglineEditText.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "You must enter your new tag line in the field above", Toast.LENGTH_SHORT).show();
                 } else {
-
                     String newTag = changeTaglineEditText.getText().toString();
                     setNewTagline(newTag);
                 }
@@ -40,7 +54,25 @@ public class settingsChangeTagline extends AppCompatActivity {
     }
 
     public void setNewTagline(String tagline) {
-        //TODO UPDATE TAGLINE IN FIREBASE WITHIN THIS FUNCTION
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(fuser.getUid());
+
+        //UPDATE TAGLINE ON DB
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("tagline", tagline);
+        reference.updateChildren(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(getApplicationContext(), "Tagline updated", Toast.LENGTH_SHORT).show();
+                changeTaglineEditText.setText("");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Tagline failed to update. Please try again later", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
 
